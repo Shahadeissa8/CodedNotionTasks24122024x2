7e79b5099b968192c68f5eb820bd306aaeb59b19
psp10 <psp10@SHAHAD>
2024-12-24 17:27:04 +0300

    Add project files.

---
+++ CodedNotionTasks24122024x2/Program.cs
@@ -0,0 +1,32 @@
+﻿using CodedNotionTasks22122024x2.models;
+
+Class1 thisIsAClass = new Class1();
+
+Console.WriteLine("enter number 1: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter number 2: ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("sum 1 is: " + thisIsAClass.Add(numb1, numb2));
+
+Console.WriteLine("enter number 3: ");
+int numb3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("sum 2 is: " + thisIsAClass.Add(numb1, numb2, numb3));
+
+Console.WriteLine("enter number 3 as a double: ");
+double numb4 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("sum 3 is: " + thisIsAClass.Add(numb1, numb2, numb4));
+
+Console.WriteLine("enter your salary: ");
+double Salary = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter your work hours: ");
+int workHours = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(thisIsAClass.salary(Salary, workHours));
+/* //doing this just in case it is required as the question is confusing "(Salary+( Work hour * hour rate)=10 KD)"
+Console.WriteLine("enter your salary: ");
+double Sal = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter your work hours: ");
+int workHrs = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter your hour rate: ");
+double hrRate = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(thisIsAClass.sal(Sal,workHrs,hrRate));
+*/
---
+++ CodedNotionTasks24122024x2/models/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodedNotionTasks22122024x2.models
+{
+    internal class Class1
+    {
+        public int Num1;
+        public int Num2;
+        public int Num3;
+        public int Add(int numb1, int numb2)
+        {
+            int sum = numb1 + numb2;
+            return sum;
+        }
+        public int Add(int numb1, int numb2, int numb3)
+        {
+            int sum = numb1 + numb2 + numb3;
+            return sum;
+        }
+        public double Add(int numb1, int numb2, double numb3)
+        {
+            double sum = numb1 + numb2 + numb3;
+            return sum;
+        }
+        public double salary(double Salary, int workHours)
+        {
+            int hourRate = 10;
+            double thisSalary = Salary + (workHours * hourRate);
+            return thisSalary;
+        }
+        /* //doing this just in case it is required as the question is confusing "(Salary+( Work hour * hour rate)=10 KD)"
+        public double sal(double Sal, int workHrs, double hrRate)
+        {
+            double salary2 = Sal+ (workHrs*hrRate); //doing this just in case it is required as the question is confusing "(Salary+( Work hour * hour rate)=10 KD)"
+            return salary2;
+        }
+        */
+    }
+
+
+}
